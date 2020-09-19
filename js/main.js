87bf33a21fcc6c3abab3ff2aaea50281443b145b
飯田恭子 <iidakyouko@mba.local>
2020-09-19 11:44:22 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,12 @@
-// コンポーネント（よく使うUIの一部を共通化）
-// Vue.component('hello-component',{
-//   template: '<p>Hello</p>'
-// })
-// フィルタ（グローバルフィルタ）
-Vue.filter('numberFormat',function(value){
-  return value.toLocaleString()
-})
-
-
 // new Vue({ })で空のビューインスタンス作成。var app =はつけてもつけなくてもいい。慣例としてappやvmが使われる。ビューインスタンスを変数化（var 変数名)することでコンソールからアクセスできる
 var app = new Vue({ 
   // #appの部分が対象
   el: '#app',
   data:{
-    message: 'Hello,vue.js!',
-    price:298000
-  },
-  methods:{
-    clickHandler:function(event){
-      // デバッグ
-      // alert('テスト);
-      this.message = this.message.split('').reverse().join('')
-    }
-  },
-  // フィルタ（ローカルフィルタ）
-  filters:{
-    numberFormat: function(value){
-      return value.toLocaleString()
-    }
+    // ◆v-if とv-else
+    // ◆v-show 
+    toggle:true,
+    // ◆v-else-if
+    color:'green'
   }
 })
